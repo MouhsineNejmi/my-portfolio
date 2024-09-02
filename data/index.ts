dada64364d1ce29c24de78be967c50287471b30b
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-09-02 11:45:30 +0100

    My Portfolio

---
+++ data/index.ts
@@ -0,0 +1,160 @@
+export const navItems = [
+  { name: 'About', link: '#about' },
+  { name: 'Projects', link: '#projects' },
+  { name: 'Testimonials', link: '#testimonials' },
+  { name: 'Contact', link: '#contact' },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      'Leveraging agile practices to deliver impactful and user-centric web solutions.',
+    description: '',
+    className: 'lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]',
+    imgClassName: 'w-full h-full',
+    titleClassName: 'justify-end',
+    img: '/b1.svg',
+    spareImg: '',
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: '',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+    imgClassName: '',
+    titleClassName: 'justify-start',
+    img: '',
+    spareImg: '',
+  },
+  {
+    id: 3,
+    title: 'My tech stack',
+    description: 'I constantly try to improve',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+    imgClassName: '',
+    titleClassName: 'justify-center',
+    img: '',
+    spareImg: '',
+  },
+  {
+    id: 4,
+    title: 'Tech enthusiast with a passion for development.',
+    description: '',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+    imgClassName: '',
+    titleClassName: 'justify-start',
+    img: '/grid.svg',
+    spareImg: '/b4.svg',
+  },
+
+  {
+    id: 5,
+    title: 'Currently building FlexStay (Node.js Microservices, Next.js)',
+    description: 'The Inside Scoop',
+    className: 'md:col-span-3 md:row-span-2',
+    imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
+    titleClassName: 'justify-center md:justify-start lg:justify-center',
+    img: '/b5.svg',
+    spareImg: '/grid.svg',
+  },
+  {
+    id: 6,
+    title: 'Are you looking for a skilled software developer?',
+    description: '',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+    imgClassName: '',
+    titleClassName: 'justify-center md:max-w-full max-w-60 text-center',
+    img: '',
+    spareImg: '',
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: 'Flex Stay - Airbnb Clone',
+    des: 'An Airbnb clone built using a microservices architecture, Flex Stay enables users to search, book, and manage property rentals with ease.',
+    img: '/airbnb-clone.webp',
+    iconLists: [
+      '/next.svg',
+      '/tail.svg',
+      '/ts.svg',
+      '/dock.svg',
+      '/git.svg',
+      '/node.svg',
+    ],
+    link: '',
+  },
+  {
+    id: 2,
+    title: 'Yoom - Video Conferencing App',
+    des: 'Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.',
+    img: '/p2.svg',
+    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/stream.svg', '/c.svg'],
+    link: '/ui.yoom.com',
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: 'cloudinary',
+    img: '/cloud.svg',
+    nameImg: '/cloudName.svg',
+  },
+  {
+    id: 2,
+    name: 'appwrite',
+    img: '/app.svg',
+    nameImg: '/appName.svg',
+  },
+  {
+    id: 3,
+    name: 'HOSTINGER',
+    img: '/host.svg',
+    nameImg: '/hostName.svg',
+  },
+  {
+    id: 4,
+    name: 'stream',
+    img: '/s.svg',
+    nameImg: '/streamName.svg',
+  },
+  {
+    id: 5,
+    name: 'docker.',
+    img: '/dock.svg',
+    nameImg: '/dockerName.svg',
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: 'FullStack Intern (Next.js - Express.js)',
+    desc: 'Assisted in the development of a web-based platform using Next.js, enhancing interactivity, and also design api using Express.js',
+    className: 'md:col-span-2',
+    thumbnail: '/exp1.svg',
+  },
+  {
+    id: 2,
+    title: 'Freelance FullStack (Next.js - Express.js)',
+    desc: 'Designed and developed web app using Next.js and Express.js.',
+    className: 'md:col-span-2', // change to md:col-span-2
+    thumbnail: '/exp2.svg',
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: '/git.svg',
+    link: 'https://github.com/MouhsineNejmi',
+  },
+  {
+    id: 2,
+    img: '/link.svg',
+    link: 'https://www.linkedin.com/in/mouhsine-nejmi/',
+  },
+];
